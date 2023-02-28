85fd1c254382591c0d784ecfeb3d68e5bc4fb050
jooyoung <72073045+daehan002@users.noreply.github.com>
2023-02-28 21:04:41 +0900

    study : 중간체크 및 과제 안내용 브랜치

--- App.js
+++ App.js
@@ -1,11 +1,70 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableWithoutFeedback} from  'react-native';
+import {View, Text, StyleSheet, 
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+  Linking,
+} from  'react-native';
+
+const DATA = {
+	"page": [
+		{
+			"name": "GOOGLE",
+			"url": "https://www.google.com/",
+			"mainColor": "red"
+		},
+		{
+			"name": "NAVER",
+			"url": "https://www.naver.com/",
+			"mainColor": "yellowgreen"
+		},
+		{
+			"name": "DAUM",
+			"url": "https://www.daum.net/",
+			"mainColor": "orange"
+		},
+		{
+			"name": "YAHOO",
+			"url": "https://www.yahoo.com/",
+			"mainColor": "mediumpurple"
+		},
+		{
+			"name": "BING",
+			"url": "https://www.bing.com/",
+			"mainColor": "green"
+		},
+		{
+			"name": "ZUM",
+			"url": "https://zum.com/",
+			"mainColor": "skyblue"
+		},
+		{
+			"name": "NATE",
+			"url": "https://www.nate.com/",
+			"mainColor": "red"
+		},
+	]
+}
+
+const scrollContent = ({name, url, mainColor}) => {
+  return (
+  <TouchableOpacity onPress={() => Linking.openURL(url)} style={{backgroundColor: `${mainColor}`}}>
+    <Text>Go To + {name}</Text>
+  </TouchableOpacity>
+  )
+}
 
 const App = () => {
   return (
-    <View style={styles.mainView}>
-      <Text style={styles.mainText}> 김형준바보 </Text>
-    </View>
+    <SafeAreaView style={{flex : 1}}>
+      <View>
+        <Text>검색엔진모음</Text>
+      </View>
+      <ScrollView>
+        <scrollContent url={DATA.page[0].url} mainColor={DATA.page[0].mainColor}>{DATA.page[0].name}</scrollContent>
+      </ScrollView>
+
+    </SafeAreaView>
   )
 }
 
