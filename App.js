9163ba4990cc30974b71354bcba84f558a044853
[Yeun-JIN] <[jinyeun12@naver.com]>
2023-03-02 19:17:18 +0900

    W03project

--- App.js
+++ App.js
@@ -1,130 +1,117 @@
 import React from 'react';
 import {
-  View, 
-  Image,
-  SafeAreaView,
-  Text, 
-  StyleSheet, 
-  TouchableOpacity,
-  Alert,
-  ImageBackground
- } from 'react-native';
- 
+ SafeAreaView,
+ Text,
+ ScrollView,
+ Linking,
+ TouchableOpacity,
+ View,
+ StyleSheet,
+} from 'react-native';
 
-const createAlert=(name)=>{
-  Alert.alert(
-    name,
-    name + "버튼 입니다.",
-    [
-      {text: "확인", onPress:()=>console.log("확인 버튼을 눌렀습니다")},
-      {text: "취소",style: "cancel",onPress:()=> console.log("취소 버튼을 눌렀습니다")}
-    ]
-  )
+const DATA = {
+	"page": [
+		{
+			"name": "GOOGLE",
+			"url": "https://www.google.com/",
+			"mainColor": "red"
+		},
+		{
+			"name": "NAVER",
+			"url": "https://www.naver.com/",
+			"mainColor": "yellowgreen"
+		},
+		{
+			"name": "DAUM",
+			"url": "https://www.daum.net/",
+			"mainColor": "orange"
+		},
+		{
+			"name": "YAHOO",
+			"url": "https://www.yahoo.com/",
+			"mainColor": "mediumpurple"
+		},
+		{
+			"name": "BING",
+			"url": "https://www.bing.com/",
+			"mainColor": "green"
+		},
+		{
+			"name": "ZUM",
+			"url": "https://zum.com/",
+			"mainColor": "skyblue"
+		},
+		{
+			"name": "NATE",
+			"url": "https://www.nate.com/",
+			"mainColor": "yellow"
+		},
+	]
+} 
+
+const TestText = ({children,color})=>{
+    return (
+        <View style={[styles.testContainer,{backgroundColor:`${color}`}]}>
+            <Text style={styles.TestText}>{children}</Text>
+        </View>
+    )
 }
 
-const App = () =>{
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <ImageBackground source={require('./profile/DisisProfile.png')} resizeMode='cover' style={styles.imgbackground}>
-        
-        <View style={styles.MainView}>
-          <TouchableOpacity onPress={()=> createAlert('취소')}>
-            <Image source={require('./profile/cancel.png')} style={styles.img}/>
-          </TouchableOpacity>
-          <View style={{flexDirection:'row'}}>
-            <TouchableOpacity onPress={()=>createAlert('선물하기')}>
-              <Image source={require('./profile/gift.png')} style={styles.HeaderRightButton}/>
-              </TouchableOpacity>
-            <TouchableOpacity onPress={()=>createAlert('QR코드')}>
-              <Image source={require('./profile/QRcode.png')} style={styles.HeaderRightButton}/>
+    <SafeAreaView style={{flex:1,justifyContent:'center',alignContent:'center'}}>
+        <ScrollView>
+            <Text style={styles.word}>검색 엔진 모음</Text>
+            <TouchableOpacity onPress={()=>Linking.openURL(DATA.page[0].url)}>
+                <TestText color={(DATA.page[0].mainColor)}>Go to Google</TestText>
             </TouchableOpacity>
-            <TouchableOpacity onPress={()=>createAlert('설정')}>
-              <Image source={require('./profile/ediet.png')} style={styles.img}/>
+            <TouchableOpacity onPress={()=>Linking.openURL(DATA.page[1].url)}>
+                <TestText color={(DATA.page[1].mainColor)}>Go to Naver</TestText>
             </TouchableOpacity>
-          </View>
-        </View>
-        
-        <View style={styles.contentView}>
-          <View style={{alignItems:"center",margin:10}}>
-            <TouchableOpacity onPress={()=>createAlert('프로필 사진')}>
-              <Image source={require('./profile/DisisProfile.png')} style={styles.ProfileImg}/>
+            <TouchableOpacity onPress={()=>Linking.openURL(DATA.page[2].url)}>
+                <TestText color={(DATA.page[2].mainColor)}>Go to DAUM</TestText>
             </TouchableOpacity>
-            <Text style={{fontSize: 20, marginTop:10}}>진예운</Text>
-          </View>
-        </View>
-        <View style={styles.Bottom}>
-          <TouchableOpacity onPress={() => createAlert('나와의 채팅')}>
-            <Image source={require('./profile/chat.png')} style={styles.bottomImg}/>
-            <Text style={styles.bottomText}>나와의 채팅</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => createAlert('프로필 편집')}>
-            <Image source={require('./profile/ediet.png')} style={styles.bottomImg}/>
-            <Text style={styles.bottomText}>프로필 편집</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => createAlert('카카오스토리')}>
-            <Image source={require('./profile/kakaostory.png')} style={styles.bottomImg}/>
-            <Text style={styles.bottomText}>카카오스토리</Text>
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
+            <TouchableOpacity onPress={()=>Linking.openURL(DATA.page[3].url)}>
+                <TestText color={(DATA.page[3].mainColor)}>Go to YAHOO</TestText>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>Linking.openURL(DATA.page[4].url)}>
+                <TestText color={(DATA.page[4].mainColor)}>Go to BING</TestText>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>Linking.openURL(DATA.page[5].url)}>
+                <TestText color={(DATA.page[5].mainColor)}>Go to ZUM</TestText>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>Linking.openURL(DATA.page[6].url)}>
+                <TestText color={(DATA.page[6].mainColor)}>Go to NATE</TestText>
+            </TouchableOpacity>
+        </ScrollView>
     </SafeAreaView>
-  
+   
   )
 }
-const styles = StyleSheet.create({
-  
-  
 
-  img:{
-    height: 25,
-    width: 25,
-    justifyContent:'center',
-    resizeMode:'contain'
-  },
-  imgbackground:{
-    width:'100%',
-    height:'100%',
-    resizeMode:"cover",
-    backgroundColor:'skyblue'
-  },
-  MainView:{
-    flexDirection:'row',
-    justifyContent:'space-between',
-    padding: 10
-  },
-  HeaderRightButton : {
-    margin : 5,
-    height : 25,
-    width : 25,
-    resizeMode: 'contain'
-  },
-  ProfileImg:{
-    height:100,
-    width: 100,
-    borderRadius : 35
-  },
-  contentView : {
-    height : 600,
-    flexDirection : 'column-reverse',
-    alignItems : 'center',
-    borderBottomColor : 'white',
-    borderBottomWidth : 1
-  },
-  Bottom : {
-    flex : 1, 
-    padding : 10, 
-    justifyContent : 'space-around', 
-    flexDirection : 'row'
-  },
-  bottomImg : {
-    width : 35,
-    height : 35,
-    alignSelf : 'center',
-  },
-  bottomText : {
-    marginTop : 3,
-    color : 'white'
-  }
+const styles = StyleSheet.create({
+    word:{
+        flex:1,
+        fontSize:30,
+        margin:20,
+        color:'black',
+        fontWeight:'900',
+        textAlign:'center'
+    },
+    testContainer:{
+        borderRadius:30,
+        margin:30,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    TestText:{
+        fontSize:25,
+        marginVertical:10,
+        marginHorizontal:20,
+        color:'black'
+    },
+    
 }
-);
+)
+
 export default App;
