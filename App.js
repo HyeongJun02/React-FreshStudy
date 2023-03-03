1d1940cf78c39a190209bac14a56231e37885866
2taeyeop <xoduq0208@naver.com>
2023-03-03 16:15:35 +0900

    week3_taeyeop

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children } from 'react';
 import {
   View, 
   Image,
@@ -8,138 +8,124 @@ import {
   TouchableOpacity,
   Alert,
   ImageBackground,
-  Touchable
+  Touchable,
+  ScrollView,
+  Linking
  } from 'react-native';
- 
 
-const createAlert=( name )=>{
-  Alert.alert(
-    name,
-    name + " 버튼 입니다.",
-    [
-      {text: "확인", onPress:()=>console.log("확인 버튼을 눌렀습니다")},
-      {text: "취소",style: "cancel",onPress:()=> console.log("취소 버튼을 눌렀습니다")}
-    ]
+
+const Engine = ({ children, color, url }) => {
+  return(
+    <TouchableOpacity onPress={() => Linking.openURL('${children}')} style ={{backgroundColor: ('${color}'), borderRadius: 20, padding: 20}}>
+      <Text style = {{fontSize : 20, color: 'black'}}>{children}</Text>
+    </TouchableOpacity>
   )
 }
 
 const App = () =>{
   return (
-    <SafeAreaView style={styles.container}>
-
-      <ImageBackground source={require('./image/taeyeop.jpg')} resizeMode='cover' style={styles.imgbackground}>
-
-        <View style={styles.headerView}>
-
-          <TouchableOpacity onPress={() => createAlert('취소')}>
-            <Image source={require('./image/cancel.png')} style={styles.img}/>
-          </TouchableOpacity>
-
-          <View style={{flexDirection : 'row'}}>
-            <TouchableOpacity onPress={() => createAlert('선물하기')}>
-              <Image source={require('./image/gift.png')} style={styles.headerRightButton}/>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => createAlert('QR코드')}>
-              <Image source={require('./image/qr.png')} style={styles.headerRightButton}/>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => createAlert('설정')}>
-              <Image source={require('./image/setting.png')} style={styles.headerRightButton}/>
-            </TouchableOpacity>
-
-          </View>
-
+    <SafeAreaView style ={{flex:1}}>
+      <ScrollView>
+        <View style = {styles.title}>
+          <Text style ={{fontSize : 20}}>검색 엔진 모음</Text>
         </View>
 
-        <View style={styles.contentView}>
+        <Engine color = {DATA.page[0].mainColors}>{DATA.page[0].name}</Engine>
 
-          <View style={{alignItems : 'center', margin : 10}}>
-            <TouchableOpacity onPress={() => createAlert('프로필 사진')}>
-              <Image source={require('./image/profile.jpg')} style={styles.profileImage}/>
-            </TouchableOpacity>
-            <Text style={{fontSize : 20, marginTop : 10, color : 'white'}}>이태엽</Text>
-          </View>
+        <TouchableOpacity onPress={() => Linking.openURL(DATA.page[0].url)} style ={[styles.button, {backgroundColor: DATA.page[0].mainColor}]}>
+          <Text style = {styles.text}>{DATA.page[0].name}</Text>
+        </TouchableOpacity>
 
-        </View>
+        <TouchableOpacity onPress={() => Linking.openURL(DATA.page[1].url)} style ={[styles.button, {backgroundColor: DATA.page[1].mainColor}]}>
+          <Text style = {styles.text}>{DATA.page[1].name}</Text>
+        </TouchableOpacity>
 
-        <View style={styles.footer}>
 
-          <TouchableOpacity onPress={() => createAlert('나와의 채팅')}>
-            <Image source={require('./image/chat.png')} style={styles.footerImg}/>
-            <Text style={styles.footerText}>나와의 채팅</Text>
-          </TouchableOpacity>
+        <TouchableOpacity onPress={() => Linking.openURL(DATA.page[2].url)} style ={[styles.button, {backgroundColor: DATA.page[2].mainColor}]}>
+          <Text style = {styles.text}>{DATA.page[2].name}</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => createAlert('프로필 편집')}>
-            <Image source={require('./image/pencil.png')} style={styles.footerImg}/>
-            <Text style={styles.footerText}>프로필 편집</Text>
-          </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => createAlert('카카오스토리')}>
-            <Image source={require('./image/story.png')} style={styles.footerImg}/>
-            <Text style={styles.footerText}>카카오스토리</Text>
-          </TouchableOpacity>
+        <TouchableOpacity onPress={() => Linking.openURL(DATA.page[3].url)} style ={[styles.button, {backgroundColor: DATA.page[3].mainColor}]}>
+          <Text style = {styles.text}>{DATA.page[3].name}</Text>
+        </TouchableOpacity>
 
-        </View>
 
-      </ImageBackground>
+        <TouchableOpacity onPress={() => Linking.openURL(DATA.page[4].url)} style ={[styles.button, {backgroundColor: DATA.page[4].mainColor}]}>
+          <Text style = {styles.text}>{DATA.page[4].name}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => Linking.openURL(DATA.page[5].url)} style ={[styles.button, {backgroundColor: DATA.page[5].mainColor}]}>
+          <Text style = {styles.text}>{DATA.page[5].name}</Text>
+        </TouchableOpacity>
+
+        
+
+      </ScrollView>
+      
     </SafeAreaView>
-  
   )
 }
 const styles = StyleSheet.create({
+  title: {
+    flex : 10,
+    justifyContent : "center",
+    alignItems : "center",
+    padding: 20,
+    margin: 20
+  },
   
-  img:{
-    height: 25,
-    width: 25,
-    justifyContent:'center',
-    resizeMode:'contain'
+  button: {
+    borderRadius: 20, 
+    padding: 20,
+    margin: 40
   },
-  imgbackground:{
-    width:'100%',
-    height:'100%',
-    resizeMode:"cover", 
-    backgroundColor:'skyblue',
-  },
-  headerView : {
-    flexDirection : 'row',
-    justifyContent : 'space-between',
-    padding : 10
-  },
-  headerRightButton : {
-    margin : 5,
-    height : 25,
-    width : 25,
-    resizeMode: 'contain'
-  },
-  profileImage : {
-    height : 100,
-    width : 100,
-    borderRadius : 35
-  },
-  contentView : {
-    height : 650,
-    flexDirection : 'column-reverse',
-    alignItems : 'center',
-    borderBottomColor : 'white',
-    borderBottomWidth : 1
-  },
-  footer : {
-    flex : 1, 
-    padding : 10, 
-    justifyContent : 'space-around', 
-    flexDirection : 'row'
-  },
-  footerImg : {
-    width : 20,
-    height : 20,
-    alignSelf : 'center',
-  },
-  footerText : {
-    marginTop : 3,
-    color : 'white'
+
+  text: {
+    fontSize : 20,
+    textAlign: 'center'
   }
   
 }
 );
+
+const DATA = {
+	"page": [
+		{
+			"name": "GOOGLE",
+			"url": "https://www.google.com/",
+			"mainColor": "red"
+		},
+		{
+			"name": "NAVER",
+			"url": "https://www.naver.com/",
+			"mainColor": "yellowgreen"
+		},
+		{
+			"name": "DAUM",
+			"url": "https://www.daum.net/",
+			"mainColor": "orange"
+		},
+		{
+			"name": "YAHOO",
+			"url": "https://www.yahoo.com/",
+			"mainColor": "mediumpurple"
+		},
+		{
+			"name": "BING",
+			"url": "https://www.bing.com/",
+			"mainColor": "green"
+		},
+		{
+			"name": "ZUM",
+			"url": "https://zum.com/",
+			"mainColor": "skyblue"
+		},
+		{
+			"name": "NATE",
+			"url": "https://www.nate.com/",
+			"mainColor": "red"
+		},
+	]
+}
 export default App;
