13c8a00bf739609514379e9db73448324c95c8b5
Yoongdi <101148622+Yoongdi@users.noreply.github.com>
2023-03-20 20:34:20 +0900

    kakaotalk chat list(flat list)

--- App.js
+++ App.js
@@ -7,8 +7,8 @@ import {
   Text,
   TouchableOpacity,
   Alert,
-  ScrollView,
   Linking,
+  FlatList
 } from 'react-native';
 
 import search from './img/search.png'
@@ -16,62 +16,172 @@ import chatPlus from './img/chatPlus.png'
 import music from './img/music.png'
 import setting from './img/setting.png'
 import thisislogo from './img/thisislogo.png'
-const DATA = {
-	"page": [
-		{ "profile": "" ,
-			"name": "하윤지",
-			"message": "다 놀았니? 할 일을 하자~~!",
-      "date":"1분전"
-     
-		},
-    {
-			"name": "이태엽",
-			"message": "태엽아 술 그만 마셔",
-      "date":"20:28"
-		},
-    {
-			"name": "진예운",
-			"message": "안농 안농",
-      "date":"17:28"
-		},
-    {
-			"name": "정주영",
-			"message": "안녕하세유..부족하지만 잘 부탁드립니다.",
-      "date":"16:30"
-		},
-    {
-			"name": "김형준",
-			"message": "? 배신자",
-      "date":"16:00"
-		},
-    {
-			"name": "어쩌구",
-			"message": "어쩌궁",
-      "date":"어제"
-		},
-    {
-			"name": "저쩌구",
-			"message": "저쩌궁",
-      "date":"3월 2일"
-		},
-    {
-			"name": "마라탕",
-			"message": "먹고싶다",
-      "date":"2월 28일"
-		},
-    {
-			"name": "마카롱",
-			"message": "맛있겠다",
-      "date":"1년전"
-		},
 
-    {
-			"name": "끝",
-			"message": "끝이다..",
-      "date":"2년전"
-		},
-	]
-}
+const DATA = [
+  {
+      id: 1,
+      title: "박경민",
+      description: "디스이즈 리액트 네이티브팀 합격하셨습니다! 축하드립니다!!",
+      date: "어제"
+  },
+  {
+      id: 2,
+      title: "정주영",
+      description: "안녕하세요! 디스이즈 총팀장 정주영입니다.",
+      date: "20:28"
+  },
+  {
+      id: 3,
+      title: "한재희",
+      description: "안녕하세요! 디스이즈 리액트 네이티브팀 15기 한재희입니다!!",
+      date: "3월 10일"
+  },
+  {
+      id: 4,
+      title: "김재원",
+      description: "안녕하세요! 디스이즈 리액트 네이티브팀 14기 김재원입니다아!",
+      date: "19:20"
+  },
+  {
+      id: 5,
+      title: "이호준",
+      description: "안녕하세요! 디스이즈 리액트 네이티브팀 3기 이호준입니당",
+      date: "19:08"
+  },
+  {
+      id: 6,
+      title: "김예나",
+      description: "안녕하세용 디스이즈 리액트 네이티브팀 15기 김예나입니돠!",
+      date: "18:59"
+  },
+  {
+      id: 7,
+      title: "조영훈",
+      description: "안녕하세요 디스이즈 서버팀 14기 조영훈입니당",
+      date: "18:50"
+  },
+  {
+      id: 8,
+      title: "김예빈",
+      description: "안녕하세요오! 디스이즈 디자인팀 14기 김예빈입니다!",
+      date: "18:46"
+  },
+  {
+      id: 9,
+      title: "황은화",
+      description: "안녕하세요! 디스이즈 마케팅팀 팀장 황은화입니당",
+      date: "18:31"
+  },
+  {
+      id: 10,
+      title: "박경민",
+      description: "디스이즈 리액트 네이티브팀 합격하셨습니다! 축하드립니다!!",
+      date: "어제"
+  },
+  {
+      id: 11,
+      title: "정주영",
+      description: "안녕하세요! 디스이즈 총팀장 정주영입니다.",
+      date: "20:28"
+  },
+  {
+      id: 12,
+      title: "한재희",
+      description: "안녕하세요! 디스이즈 리액트 네이티브팀 15기 한재희입니다!!",
+      date: "3월 10일"
+  },
+  {
+      id: 13,
+      title: "김재원",
+      description: "안녕하세요! 디스이즈 리액트 네이티브팀 14기 김재원입니다아!",
+      date: "19:20"
+  },
+  {
+      id: 14,
+      title: "이호준",
+      description: "안녕하세요! 디스이즈 리액트 네이티브팀 3기 이호준입니당",
+      date: "19:08"
+  },
+  {
+      id: 15,
+      title: "김예나",
+      description: "안녕하세용 디스이즈 리액트 네이티브팀 15기 김예나입니돠!",
+      date: "18:59"
+  },
+  {
+      id: 16,
+      title: "조영훈",
+      description: "안녕하세요 디스이즈 서버팀 14기 조영훈입니당",
+      date: "18:50"
+  },
+  {
+      id: 17,
+      title: "김예빈",
+      description: "안녕하세요오! 디스이즈 디자인팀 14기 김예빈입니다!",
+      date: "18:46"
+  },
+  {
+      id: 18,
+      title: "황은화",
+      description: "안녕하세요! 디스이즈 마케팅팀 팀장 황은화입니당",
+      date: "18:31"
+  },
+  {
+      id: 19,
+      title: "박경민",
+      description: "디스이즈 리액트 네이티브팀 합격하셨습니다! 축하드립니다!!",
+      date: "어제"
+  },
+  {
+      id: 20,
+      title: "정주영",
+      description: "안녕하세요! 디스이즈 총팀장 정주영입니다.",
+      date: "20:28"
+  },
+  {
+      id: 21,
+      title: "한재희",
+      description: "안녕하세요! 디스이즈 리액트 네이티브팀 15기 한재희입니다!!",
+      date: "3월 10일"
+  },
+  {
+      id: 22,
+      title: "김재원",
+      description: "안녕하세요! 디스이즈 리액트 네이티브팀 14기 김재원입니다아!",
+      date: "19:20"
+  },
+  {
+      id: 23,
+      title: "이호준",
+      description: "안녕하세요! 디스이즈 리액트 네이티브팀 3기 이호준입니당",
+      date: "19:08"
+  },
+  {
+      id: 24,
+      title: "김예나",
+      description: "안녕하세용 디스이즈 리액트 네이티브팀 15기 김예나입니돠!",
+      date: "18:59"
+  },
+  {
+      id: 25,
+      title: "조영훈",
+      description: "안녕하세요 디스이즈 서버팀 14기 조영훈입니당",
+      date: "18:50"
+  },
+  {
+      id: 26,
+      title: "김예빈",
+      description: "안녕하세요오! 디스이즈 디자인팀 14기 김예빈입니다!",
+      date: "18:46"
+  },
+  {
+      id: 27,
+      title: "황은화",
+      description: "안녕하세요! 디스이즈 마케팅팀 팀장 황은화입니당",
+      date: "18:31"
+  }
+];
+
 const createAlert0=( name )=>{
   Alert.alert(
     name,
@@ -82,16 +192,33 @@ const createAlert0=( name )=>{
     ]
   )
 }
-const createAlert1=( name )=>{
+const createAlert1=( title )=>{
   Alert.alert(
-    name,
-    name + "님 과의 채팅 버튼입니다.",
+    title,
+    title + "님 과의 채팅 버튼입니다.",
     [
       {text: "확인", onPress:()=>console.log("확인 버튼을 눌렀습니다")},
       {text: "취소",style: "cancel",onPress:()=> console.log("취소 버튼을 눌렀습니다")}
     ]
   )
 }
+
+const renderItem = ({item}) => {
+  return (
+    
+    <TouchableOpacity onPress={() => createAlert1(item.title)} style={{ borderRadius: 20, padding: 10}} >
+    <Text style={{ fontSize:12, textAlign:"right"}}>{item.date}</Text>
+    <View style={{borderRadius: 25, flexDirection:"row"}}>
+      <Image source={thisislogo} style={{height: 55,width: 55,borderRadius: 15, margin:5}}></Image>
+      <View style={{}}>
+        <Text style={{ fontSize:22, margin:3}}>{item.title}</Text>
+        <Text style={{ fontSize:13,margin:3}}>{item.description}</Text>
+      </View>
+    </View>
+    </TouchableOpacity>
+  )
+}
+
 const url = "https://velog.io/@yun_ji"
 
 const App = () => {
@@ -117,33 +244,19 @@ const App = () => {
             <Image source={setting} style={{width: 20,height: 20,margin:10}}></Image>
          </TouchableOpacity>
       </View>
-      
+      <TouchableOpacity onPress={() => Linking.openURL(url)} style={{backgroundColor: '#E6E6E6', borderRadius: 20, padding: 20}}>
+       <Text style={{fontSize: 20, textAlign: 'center'}}>광고 배너</Text>
+     </TouchableOpacity>
       <View style={styles.profileView}>
-        <ScrollView>
-          <TouchableOpacity onPress={() => Linking.openURL(url)} style={{backgroundColor: '#E6E6E6', borderRadius: 20, padding: 20}}>
-            <Text style={{fontSize: 20, textAlign: 'center'}}>광고 배너</Text>
-          </TouchableOpacity>
+        <FlatList
 
-
-        { DATA.page.map(item=>{
-          return(
-            <TouchableOpacity onPress={() => createAlert1(item.name)} style={{ borderRadius: 20, padding: 10}} >
-            <Text style={{ fontSize:12, textAlign:"right"}}>{item.date}</Text>
-            <View style={{borderRadius: 25, flexDirection:"row"}}>
-              <Image source={thisislogo} style={{height: 55,width: 55,borderRadius: 15, margin:5}}></Image>
-              <View style={{}}>
-                <Text style={{ fontSize:22, margin:3}}>{item.name}</Text>
-                <Text style={{ fontSize:13,margin:3}}>{item.message}</Text>
-              </View>
-            </View>
-            </TouchableOpacity>
-          )
-        })}
-          
-        </ScrollView>
+        data = {DATA}
+        renderItem = {renderItem}
+        
+        />
       </View>
-      
     </SafeAreaView>
+        
 )
 }
 
@@ -170,7 +283,6 @@ const styles = StyleSheet.create({
     flex: 12,
     margin:10,
   },
-  
 
  
 });
