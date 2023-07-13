3bfacb2bd420f2d3391aa926c2c8d0eebe3c4383
2taeyeop <xoduq0208@naver.com>
2023-07-13 20:34:13 +0900

    asdf

--- screens/ApplicationScreen.js
+++ screens/ApplicationScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import PickerComponent from '../src/PickerComponent';
 
 function ApplicationScreen() {
     return (
-        <View style = {styles.block} />
+        <PickerComponent />
     )
 }
 
