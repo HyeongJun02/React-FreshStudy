b40b603c4729787da4f5606e24f7118eeecb1658
2taeyeop <xoduq0208@naver.com>
2023-07-13 20:28:21 +0900

    fuck

---
+++ src/PickerComponent.js
@@ -0,0 +1,23 @@
+import React, { useState} from "react";
+import { SafeAreaView, SafeAreaViewProps, Text } from "react-native-safe-area-context";
+import { Picker } from "@react-native-picker/picker";
+
+const PickerComponent = () => {
+    const [pickerValue, setPickerValue] = useState("1");
+
+    return ( 
+        <SafeAreaView>
+            <Picker
+            	selectedValue={pickerValue}
+                onValueChange={(item) => setPickerValue(item)}
+            >
+				<Picker.Item label="라벨_1" value="1" />
+				<Picker.Item label="라벨_2" value="2" />
+				<Picker.Item label="라벨_3" value="3" />
+			</Picker>        
+        	<View>
+            	<Text>{pickerValue}</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
