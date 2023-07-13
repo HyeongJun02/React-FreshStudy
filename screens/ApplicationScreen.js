4d290cd82ccc57c61d45e66baf5f34fde18ca2ba
2taeyeop <xoduq0208@naver.com>
2023-07-14 01:31:29 +0900

    124

--- screens/ApplicationScreen.js
+++ screens/ApplicationScreen.js
@@ -1,31 +1,109 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, {useState} from 'react'
+import { View, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 import RNPickerSelect from 'react-native-picker-select';
-import {Calendar} from "react-native-calendars";
 import { FullWindowOverlay } from 'react-native-screens';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
+Date.prototype.format = function(f) {
+    if (!this.valueOf()) return " ";
+ 
+    var weekName = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+    var d = this;
+     
+    return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|a\/p)/gi, function($1) {
+        switch ($1) {
+            case "yyyy": return d.getFullYear();
+            case "yy": return (d.getFullYear() % 1000).zf(2);
+            case "MM": return (d.getMonth() + 1).zf(2);
+            case "dd": return d.getDate().zf(2);
+            case "E": return weekName[d.getDay()];
+            case "HH": return d.getHours().zf(2);
+            case "hh": return ((h = d.getHours() % 12) ? h : 12).zf(2);
+            case "mm": return d.getMinutes().zf(2);
+            case "ss": return d.getSeconds().zf(2);
+            case "a/p": return d.getHours() < 12 ? "오전" : "오후";
+            default: return $1;
+        }
+    });
+};
+ 
+String.prototype.string = function(len){var s = '', i = 0; while (i++ < len) { s += this; } return s;};
+String.prototype.zf = function(len){return "0".string(len - this.length) + this;};
+Number.prototype.zf = function(len){return this.toString().zf(len);};
 
 function ApplicationScreen() {
+    <View>
+        <RNPickerSelect
+            onValueChange={(value) => console.log(value)}
+            items={[
+                { label: 'Football', value: 'football' },
+                { label: 'Baseball', value: 'baseball' },
+                { label: 'Hockey', value: 'hockey' },
+            ]}
+        />
+    </View>
+    
+    const placeholder = "날짜를 입력해주세요";
+
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    const [text, onChangeText] = useState("");
+    
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
+    };
+
+    const hideDatePicker = () => {
+        setDatePickerVisibility(false);
+    };
+
+    const handleConfirm = (date) => {
+        console.warn("dateFormat: ", date.format("yyyy/MM/dd"));
+        hideDatePicker();
+        onChangeText(date.format("yyyy/MM/dd"))
+    };
+
     return (
-        <View>
-             <RNPickerSelect
-                onValueChange={(value) => console.log(value)}
-                items={[
-                    { label: 'Football', value: 'football' },
-                    { label: 'Baseball', value: 'baseball' },
-                    { label: 'Hockey', value: 'hockey' },
-                ]}
-            />
-            <Calendar style={styles.calendar}></Calendar>
+        <View style={styles.container}>
+            <TouchableOpacity onPress={showDatePicker}>
+                <TextInput
+                    pointerEvents="none"
+                    style={styles.textInput}
+                    placeholder={placeholder}
+                    placeholderTextColor='#000000'
+                    underlineColorAndroid="transparent"
+                    editable={false}
+                    value={text}
+                />
+                <DateTimePickerModal
+                    headerTextIOS={placeholder}
+                    isVisible={isDatePickerVisible}
+                    mode="date"
+                    onConfirm={handleConfirm}
+                    onCancel={hideDatePicker}
+                />
+            </TouchableOpacity>
         </View>
-        
-    )
+  );
 }
 
-const styles = StyleSheet.create({
-    calendar: {
-        borderBottomWidth: 1,
-        borderBottomColor: "#e0e0e0",
+const styles = StyleSheet.create({ 
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
     },
-});
+    textInput: {
+        fontSize: 16,
+        color: '#000000',
+        height: 50, 
+        width: 300, 
+        borderColor: '#000000', 
+        borderWidth: 1, 
+        borderRadius: 12,
+        padding: 10
+    }
+})
+
 
 export default ApplicationScreen;
--- screens/IntroduceScreen.js
+++ screens/IntroduceScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import CalendarView from './CalendarView';
+
 
 function IntroduceScreen() {
     return (
         <View>
-            <CalendarView/>;
+
         </View>
     )
 }
